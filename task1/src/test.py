df66d475d2b1b9fc4ee5aaf31f6b26a0b7c7830c
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-04-23 01:06:04 +0900

    add: テストの追加

---
+++ task1/src/test.py
@@ -0,0 +1,26 @@
+import gurobipy as gp
+
+from make_array import make_array
+from solve import solve
+from test_module.generate_random_true import generate_random_true
+
+
+def main():
+    # test_case1
+    # 全ての要素が0の配列
+    N = 10
+    array = [[0] * (N + 2) for _ in range(N + 2)]
+    res = solve(N, array)
+    assert res
+
+    # test_case2
+    for _ in range(10):
+        N, array = generate_random_true()
+        res = solve(N, array)
+        assert res
+
+    # test_case3
+    # TODO: 解が存在しない場合のテストケースを追加する
+
+if __name__ == "__main__":
+    main()
