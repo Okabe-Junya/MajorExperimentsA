ae7593417324d6103f41e07d48a2adb4c4879d95
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-05-26 01:24:43 +0900

    dir: ディレクトリ構成の変更

---
+++ task2/src/lib/naive.py
@@ -0,0 +1,33 @@
+def naive(n, m, p, r, b):
+    """solve the problem with the naive full bit search
+
+    Args:
+        n (int): number of items
+        m (int): number of restrictions
+        p (list): list of prices
+        r (list): list of restrictions
+        b (list): list of budgets
+
+    Returns:
+        int: the maximum price under the restrictions
+    """
+    executable_solution = []
+    for num in range(2 ** n):
+        choice = []
+        check = [0] * m
+        bin_num = str(bin(num)[2:]).zfill(n)
+
+        assert len(bin_num) == n
+
+        for bit in range(n):
+            if bin_num[bit] == '1':
+                choice.append(p[bit])
+                for item in range(m):
+                    check[item] += r[item][bit]
+
+        for item in range(m):
+            if check[item] > b[item]:
+                break
+        else:
+            executable_solution.append(sum(choice))
+    return max(executable_solution)
