7b93e49154c98d0114665816d38caf1457b87f0d
Okabe-Junya <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-05-26 01:23:28 +0900

    add: 貪欲法による近似解の導出

---
+++ task2/src/lib/greedy.py
@@ -0,0 +1,25 @@
+def greedy(n, m, p, r, b):
+    """solve the problem with the greedy algorithm
+
+    Args:
+        n (int): number of items
+        m (int): number of restrictions
+        p (list): list of prices
+        r (list): list of restrictions
+        b (list): list of budgets
+
+    Returns:
+        int: the maximum price under the restrictions
+    """
+    p_with_index = [(p[i], i) for i in range(n)]
+    p_with_index.sort(reverse=True)
+    opt = 0
+    for p_index in range(n):
+        for i in range(m):
+            if r[i][p_index] > b[i]:
+                break
+        else:
+            for i in range(m):
+                b[i] -= r[i][p_index]
+            opt += p[p_index]
+    return opt
