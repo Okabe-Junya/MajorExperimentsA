b121edab55482caf0a041631e856cd2693ae167d
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-07-26 02:28:07 +0900

    task3 completed

--- task3/src/main.py
+++ task3/src/main.py
@@ -1,5 +1,34 @@
-def main():
-    pass
+import networkx as nx
+import numpy as np
 
+
+from pagerank import pagerank
+
+
+airport_code = ["HND", "NRT", "CTS", "ITM", "KIX", "FUK", "OKA"]
+
+travelers = [
+    [0, 0, 420059, 310884, 64422, 473142, 336687],
+    [0, 0, 114039, 1116, 62662, 113658, 63456],
+    [420059, 114039, 0, 66113, 85515, 42474, 9115],
+    [310884, 1116, 66113, 0, 0, 44969, 71348],
+    [64422, 62662, 85515, 0, 0, 31471, 74875],
+    [473142, 113658, 42474, 44969, 31471, 0, 122941],
+    [336687, 63456, 9115, 71348, 74875, 122941, 0]
+]
+
+
+def main():
+    G = nx.DiGraph()
+    G.add_nodes_from(airport_code)
+
+    for O in range(len(travelers)):
+        for D in range(len(travelers[O])):
+            if travelers[O][D] != 0:
+                G.add_edge(airport_code[O], airport_code[D],
+                        weight=travelers[O][D])
+                
+    print(pagerank(G))
+            
 if __name__ == '__main__':
     main()
